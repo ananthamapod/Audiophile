78401c89a817cfe8f4a5d2e6c84a6ee22d5e55c7
Ananth Rao <ananthamapod@gmail.com>
2016-04-29 13:33:20 -0400

    Implement hard coded movement patter for proof of concept demo

--- Python/main.py
+++ Python/main.py
@@ -5,6 +5,7 @@ from analysis.audio import Audio
 from client import Audiophile_Client
 from operator import itemgetter
 import time
+import math
 
 def handshake(ser):
     """ Serial handshake with Arduino to get the connection going """
@@ -16,11 +17,10 @@ def handshake(ser):
         time.sleep(2)
 
 def act(intention, ser, audio, plotter, client):
-    """ Deciphers intended action from intention, writes to serial,
+	""" Deciphers intended action from intention, writes to serial,
     records audio, and plots respectively based on action parameters """
-    c = intention.command
-    v = intention.value
-    print c
+	c = intention.command
+	v = intention.value
 	if c == "RECORD":
 		print("3")
 		time.sleep(0.5)
@@ -29,10 +29,10 @@ def act(intention, ser, audio, plotter, client):
 		print("1")
 		time.sleep(0.5)
 		audio.record(v)
-		r = client.send_data(audio.getFrames(v))
+		r = client.send_data(audio.get_frames(v), v)
 		if r == "Failure":
 			client.connect()
-			client.send_data(audio.getFrames(v))
+			client.send_data(audio.get_frames(v), v)
 #    if c == "TURN":
 #        ser.write("T: " + str(v))
 #    elif c = "MOVETO":
@@ -58,34 +58,101 @@ def run(ser, audio, agent, plotter, client):
         print inputs
 
 def setup():
-    """ Overall setting up - setting up serial connnection, set up
+	""" Overall setting up - setting up serial connnection, set up
     audio, agent, plotter, and other essentials """
-    ser = serial.Serial('/dev/ttyACM0', 9600)
-    handshake(ser)
-    audio = Audio()
-    agent = Agent()
-    plotter = Plotter()
-    plotter.configurePlots()
+	ser = serial.Serial('/dev/ttyACM0', 9600)
+	handshake(ser)
+	audio = Audio()
+	agent = Agent()
+	plotter = Plotter()
+	plotter.configurePlots()
 	client = Audiophile_Client()
-    return ser, audio, agent, plotter, client
+	return ser, audio, agent, plotter, client
 
 def finish(audio):
     """ Takes all the recorded frames and plots them by coordinate """
-    frames = audio.getFrames()
-    coordinates = sorted(frames.keys(), key=itemgetter(0,1))
+    #frames = audio.getFrames()
+    #coordinates = sorted(frames.keys(), key=itemgetter(0,1))
+    pass
 
+
+def robot_stop(currSpeed, ser):
+	while currSpeed is not 0:
+		currSpeed = int(math.floor(currSpeed - (currSpeed)/3.))
+		if currSpeed < 40:
+			currSpeed = 0
+		ser.write("FORWARD " + str(currSpeed))
+		print "s" + str(currSpeed)
+		time.sleep(2)
+	ser.write("STOP")
+	return currSpeed
+
+def robot_go(currSpeed, ser):
+	if currSpeed is not 100:
+		currSpeed = max(currSpeed, 40)
+	while currSpeed is not 100:
+		currSpeed = int(math.ceil(currSpeed + (100-currSpeed)/3.))
+		ser.write("FORWARD " + str(currSpeed))
+		print "g" + str(currSpeed)
+		time.sleep(2)
+	return currSpeed
+
+def robot_turn(angle, ser):
+	if angle > 0:
+		ser.write("RIGHT 60")
+	else:
+		ser.write("LEFT 60")
+	time.sleep(1)
+	return
 
 if __name__ == "__main__":
-    """ Tests entire setup """
-    ser, audio, agent, plotter, client = setup()
-    handshake(ser)
+	""" Tests entire setup """
+	ser, audio, agent, plotter, client = setup()
+	handshake(ser)
 	audio.open_mic()
 	client.connect()
 
-    while True:
+	# real loop for doing things the right way
+	"""
+	while True:
         try:
             run(ser, audio, agent, plotter, client)
         except KeyboardInterrupt:
             break
+	"""
+	currSpeed = 0
+	"""
+	while True:
+		inputs = ser.readline()
+		op = raw_input("command: ")
+		if op.lower() == "w":
+			currSpeed = robot_go(currSpeed, ser)
+		elif op.lower() == "a":
+			robot_turn(-90, ser)
+		elif op.lower() == "s":
+			currSpeed = robot_stop(currSpeed, ser)
+		elif op.lower() == "d":
+			robot_turn(90, ser)
+		else:
+			robot_stop(currSpeed, ser)
+			break
+	"""
+	#for j in range(2):
+	for i in range(4):
+		currSpeed = robot_go(currSpeed, ser)
+		currSpeed = robot_stop(currSpeed, ser)
+		print("3")
+		time.sleep(0.5)
+		print("2")
+		time.sleep(0.5)
+		print("1")
+		time.sleep(0.5)
+		#coords = (j, i)
+		coords = i
+		audio.record(coords)
+		r = client.send_data(audio.get_frames(coords), coords)
+		if r == "Failure":
+			client.connect()
+			client.send_data(audio.get_frames(coords), coords)
 
-    finish(audio)
+	finish(audio)
